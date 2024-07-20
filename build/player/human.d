840bceecda21e219e30e8c57bb3a15d99dfb3b6d
izy66 <r473li@uwaterloo.ca>
2024-07-20 14:41:43 -0400

    removed build directory

--- build/player/human.d
+++
@@ -1,4 +0,0 @@
-build/player/human.o: src/player/human.cc src/player/human.h \
-  src/player/player.h src/player/parser.h src/board.h src/pieces/blank.h \
-  src/pieces/square.h src/pieces/piece.h src/pieces/iterators/iterator.h \
-  src/subject.h src/player/random_player.h src/moves/move.h
